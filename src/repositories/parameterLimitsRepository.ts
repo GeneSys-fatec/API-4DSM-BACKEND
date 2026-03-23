6b08d3e6f4863ce91be72e4e18f4375678b19d50
Gabriel Medeiros <157700662+gbmedeiros00@users.noreply.github.com>
2026-03-23 11:46:48 -0300

    Revert "feat[GEN-15]: implementa API de gerenciamento de limites de parâmetros"

--- src/repositories/parameterLimitsRepository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { parameterLimitsEntity } from "../entities/parameterLimitsEntity.js";
-
-@EntityRepository(parameterLimitsEntity)
-export class parameterLimitsRepository extends Repository<parameterLimitsEntity> {}
